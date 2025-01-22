c6d4d99fb96b9d08a8191d47a1167dca1be91f9a
elmurio <murahmullaev@gmail.com>
2025-01-22 18:28:27 +0500

    Add files via upload

--- python/w3schools_tasks.py
+++ python/w3schools_tasks.py
@@ -1,8 +1,1 @@
-x = int(input("Enter some number : "))
-
-if x > 2:
-    print("Your number is greater 2")
-elif x < 2:
-    print("Your number is less than 2")
-else:
-    print("Your number is exactly 2")
+print("Hello World!")
